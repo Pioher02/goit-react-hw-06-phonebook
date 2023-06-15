22ed576593bee976e549374e0702293722c70fcd
Pioher02 <dpaola.hernandezr@gmail.com>
2023-06-14 21:34:09 -0500

    Creacion del store

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,16 +4,21 @@ import Filter from './Filter';
 import { nanoid } from 'nanoid';
 import Notiflix from 'notiflix';
 import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 
 const App = () => {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
-  const [prevContacts, setPrevContacts] = useState(contacts);
-  const [filter, setFilter] = useState('');
+    
+  const contacts = useSelector((state) => state.contacts); // para usarlo
+  const filter = useSelector((state) => state.filter);
+
+  // const [contacts, setContacts] = useState([
+  //   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  //   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  //   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  //   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  // ]);
+  // const [prevContacts, setPrevContacts] = useState(contacts);
+  // const [filter, setFilter] = useState('');
 
   const saveContact = evt => {
     evt.preventDefault();
